a6676e0be1a1cbb9b3b5f978cf297b6f483edf17
git-sunyingzhan <oneselfwechat@163.com>
2019-08-21 16:40:25 +0800

    19-08-20

---
+++ src/oop/dya04/computer/KeyBoard.java
@@ -0,0 +1,22 @@
+package oop.dya04.computer;
+
+/**
+ * KeyBoard
+ */
+public class KeyBoard implements USB {
+
+    @Override
+    public void open() {
+        System.out.println("打开键盘");
+    }
+
+    @Override
+    public void close() {
+        System.out.println("关闭键盘");
+    }
+
+    public void click() {
+        System.out.println("点击键盘");
+    }
+
+}
---
+++ src/oop/dya04/computer/USB.java
@@ -0,0 +1,11 @@
+package oop.dya04.computer;
+
+/**
+ * USB
+ */
+public interface USB {
+
+    public abstract void open();// 打开
+
+    public abstract void close();// 关闭
+}
